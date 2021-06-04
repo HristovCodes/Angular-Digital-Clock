1ab63234e51c6729c95a5c23bdd49f3e210b2bab
HristovCodes <ivailohristov1990@gmail.com>
2021-06-04 15:35:16 +0300

    Displys current time Doesn't update and doesn't support all formats

--- digitalclock/src/app/digitalclock/digitalclock.component.ts
+++ digitalclock/src/app/digitalclock/digitalclock.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./digitalclock.component.scss'],
 })
 export class DigitalclockComponent implements OnInit {
+  hours!: number;
+  hours0!: number;
+  hours1!: number;
+  minutes!: number;
+  minutes0!: number;
+  minutes1!: number;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.hours = new Date().getHours();
+    this.minutes = new Date().getMinutes();
+    this.hours0 = this.hours >= 10 ? Math.floor(this.hours / 10) : this.hours;
+    this.hours1 = this.hours % 10;
+    this.minutes0 =
+      this.minutes >= 10 ? Math.floor(this.minutes / 10) : this.minutes;
+    this.minutes1 = this.minutes % 10;
+  }
 }
