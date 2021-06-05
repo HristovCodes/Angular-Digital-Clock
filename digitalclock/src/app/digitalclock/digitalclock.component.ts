80010a0a26b648a9757742d337493cd03541da52
HristovCodes <ivailohristov1990@gmail.com>
2021-06-05 16:23:42 +0300

    Demo ready.

--- digitalclock/src/app/digitalclock/digitalclock.component.ts
+++ digitalclock/src/app/digitalclock/digitalclock.component.ts
@@ -6,59 +6,51 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./digitalclock.component.scss'],
 })
 export class DigitalclockComponent implements OnInit, OnDestroy {
-  format: string = 'hh:mm:ss:f';
+  format: string = 'H:mm:ss:t';
   hourformat: string = this.format.split(':')[0];
   minuteformat: string = this.format.split(':')[1];
   secsformat: string = this.format.split(':')[2];
   ampmbool: string = this.format.split(':')[3];
 
-  hours!: number;
   hours0!: number;
   hours1!: number;
-  minutes!: number;
   minutes0!: number;
   minutes1!: number;
-  secs!: number;
   secs0!: number;
   secs1!: number;
   ampm!: string;
   intervalID!: number;
 
   updateTime(): void {
-    this.hours = new Date().getHours();
-    this.minutes = new Date().getMinutes();
-    this.secs = new Date().getSeconds();
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let secs = new Date().getSeconds();
 
-    if (this.ampmbool === 't') {
-      this.ampm = 'am';
-      if (this.hours > 12) {
-        this.hours -= 12;
-        this.ampm = 'pm';
-      }
-    }
+    this.ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours === 0 ? 12 : hours % 12;
 
     if (this.hourformat === 'h' || this.hourformat === 'H') {
-      this.hours0 = this.hours >= 10 ? Math.floor(this.hours / 10) : 10;
-      this.hours1 = this.hours % 10;
+      this.hours0 = hours >= 10 ? Math.floor(hours / 10) : 10;
+      this.hours1 = hours % 10;
     } else {
-      this.hours0 = this.hours >= 10 ? Math.floor(this.hours / 10) : 0;
-      this.hours1 = this.hours % 10;
+      this.hours0 = hours >= 10 ? Math.floor(hours / 10) : 0;
+      this.hours1 = hours % 10;
     }
 
     if (this.hourformat === 'm') {
-      this.minutes0 = this.minutes >= 10 ? Math.floor(this.minutes / 10) : 10;
-      this.minutes1 = this.minutes % 10;
+      this.minutes0 = minutes >= 10 ? Math.floor(minutes / 10) : 10;
+      this.minutes1 = minutes % 10;
     } else {
-      this.minutes0 = this.minutes >= 10 ? Math.floor(this.minutes / 10) : 0;
-      this.minutes1 = this.minutes % 10;
+      this.minutes0 = minutes >= 10 ? Math.floor(minutes / 10) : 0;
+      this.minutes1 = minutes % 10;
     }
 
     if (this.secsformat === 's') {
-      this.secs0 = this.secs >= 10 ? Math.floor(this.secs / 10) : 10;
-      this.secs1 = this.secs % 10;
+      this.secs0 = secs >= 10 ? Math.floor(secs / 10) : 10;
+      this.secs1 = secs % 10;
     } else {
-      this.secs0 = this.secs >= 10 ? Math.floor(this.secs / 10) : 0;
-      this.secs1 = this.secs % 10;
+      this.secs0 = secs >= 10 ? Math.floor(secs / 10) : 0;
+      this.secs1 = secs % 10;
     }
   }
 
--- digitalclock/src/app/digitalclock/digitalclock.module.ts
+++ digitalclock/src/app/digitalclock/digitalclock.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 
 import { DigitComponent } from '../digit/digit.component';
 import { DigitalclockComponent } from './digitalclock.component';
+import { HristovDigitalClockModule } from 'hristov-digital-clock';
 
 @NgModule({
   declarations: [DigitComponent, DigitalclockComponent],
-  imports: [CommonModule, BrowserModule],
+  imports: [CommonModule, BrowserModule, HristovDigitalClockModule],
   exports: [DigitComponent],
   bootstrap: [DigitalclockComponent],
 })
